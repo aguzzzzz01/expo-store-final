65c8495e82ad47667982f205fba3ec0ab44ab803
aguzzzzz01 <85626183+aguzzzzz01@users.noreply.github.com>
2026-06-07 21:39:36 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  tanstackStart: {
-    ssr: false
-  },
-
   vite: {
     build: {
       target: "esnext"
