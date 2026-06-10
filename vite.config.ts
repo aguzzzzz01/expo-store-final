020c8b90990302222121830562fc3e2043870ccf
aguzzzzz01 <85626183+aguzzzzz01@users.noreply.github.com>
2026-06-10 01:25:29 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths()
+    tsconfigPaths(),
+    // Esto resuelve el error de "node:async_hooks" dándole soporte en el navegador
+    nodePolyfills({
+      include: ["async_hooks"],
+    }),
   ],
 });
-
