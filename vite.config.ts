a611906e27ba8451cd22c7073af8dee4719a1ec0
aguzzzzz01 <85626183+aguzzzzz01@users.noreply.github.com>
2026-06-10 01:40:40 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,20 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths()
-  ],
+    tsconfigPaths(),
+    {
+      name: "bypass-async-local-storage",
+      enforce: "pre",
+      resolveId(id) {
+        if (id === "node:async_hooks") {
+          return "\0node:async_hooks";
+        }
+      },
+      load(id) {
+        if (id === "\0node:async_hooks") {
+          return `export class AsyncLocalStorage { disable() {} enable() {} enterWith() {} run() {} getStore() {} };`;
+        }
+      }
+    }
+  ]
 });
