41e273af32108d34a5e3637165977e68dba962be
aguzzzzz01 <85626183+aguzzzzz01@users.noreply.github.com>
2026-06-10 01:14:59 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import path from "path";
 
 export default defineConfig({
-  // Le dice a Vite que todo tu proyecto real está en la carpeta 'versionfinal'
-  root: "versionfinal",
   plugins: [
     react(),
     tsconfigPaths()
   ],
-  build: {
-    // Saca el resultado limpio a la carpeta dist principal que espera Cloudflare
-    outDir: path.resolve(__dirname, "dist"),
-    emptyOutDir: true
-  }
 });
 
